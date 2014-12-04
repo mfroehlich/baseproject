c74fd2f20c050e565fa52bffa59ed1c08671de28
Markus Fröhlich <markusfroehlich1@gmx.de>
2014-12-04 08:11:38 +0100

    Einbinden des ErrorCollectorService in die Aufrufe des Loggers. Lediglich die Anmeldung der MBean funktioniert noch nicht.

--- baseproject-commons/src/main/java/com/froehlich/commons/producer/LoggerProducer.java
+++ baseproject-commons/src/main/java/com/froehlich/commons/producer/LoggerProducer.java
@@ -1,19 +1,26 @@
 package com.froehlich.commons.producer;
 
+import com.froehlich.commons.monitoring.entity.ErrorCollectingLogger;
+import com.froehlich.commons.monitoring.service.ErrorCollectorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
 
 /**
  * Created by mfroehlich on 03.12.2014.
  */
 public class LoggerProducer {
 
+    @Inject
+    private ErrorCollectorService errorCollectorService;
+
     @Produces
     public Logger produceLogger(InjectionPoint injectionPoint) {
         Logger logger = LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
-        return logger;
+        ErrorCollectingLogger errorCollectingLogger = new ErrorCollectingLogger(logger, errorCollectorService);
+        return errorCollectingLogger;
     }
 }
